5248df76700a2aa57d3c950c5f8820bc6217c84d
ignacio-focas <ignacio.focas@moxit.com.ar>
2022-12-05 13:34:42 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Draggable from 'react-draggable';
+import { Button, Menu } from 'semantic-ui-react';
+
 
 function App() {
+
+  const items= ["item 1", "item 2", "item 3", "item 4"];
+  const [positions, setPositions] = useState({})
+  const [hasLoaded, setHasLoaded] = useState(false)
+  const [disableDrag, setDisableDrag] = useState(true)
+
+  useEffect(()=>{
+    const existingButtonPositions = JSON.parse(localStorage.getItem('positions_button'))
+    setPositions(existingButtonPositions);
+    setHasLoaded(true);
+  },[])
+
+  function handleStop(e, data){
+    let dummyPositions = {...positions}
+    const itemId = e.target.id
+    dummyPositions[itemId] = {}
+    dummyPositions[itemId]["x"] = data.x
+    dummyPositions[itemId]["y"] = data.y
+    setPositions(dummyPositions)
+  }
+
+  useEffect(()=>{
+    localStorage.setItem('positions_button', JSON.stringify(positions))
+  },[positions])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    hasLoaded ? (
+      <>
+      <Menu>
+       <Menu.Item>
+        <Button.Group>
+          <Button onClick={()=>{setDisableDrag(false)}}>Editar</Button>
+          <Button.Or />
+          <Button onClick={()=>{setDisableDrag(true)}}>Ver</Button>
+        </Button.Group>
+       </Menu.Item>
+      </Menu>
+      
+      <div className='itemsContainer'>
+          {items.map((item)=>{
+            return(
+              <Draggable disabled={disableDrag} key={item[5]} defaultPosition={
+                positions === null ? 
+                {x:0, y:0} 
+                : !positions[item[5]] ? 
+                {x: 0, y: 0} 
+                :{x:positions[item[5]].x, y: positions[item[5]].y} 
+                }
+                onStop={handleStop}
+                bounds='parent'>
+              
+                <Button id={item[5]} className='itemDrag' >{item}</Button>
+                
+              </Draggable>
+            )
+          })}
+      </div>
+      </>
+    ) : null
   );
 }
 
